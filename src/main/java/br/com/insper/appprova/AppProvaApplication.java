14ffe5cfd9c968b28fa28009436ec59f1d915e00
Giovanni Vilella Rodrigues <givilella37@gmail.com>
2026-09-23 08:32:20 -0300

    setup de docker, jacoco e pipeline de deploy

---
+++ src/main/java/br/com/insper/appprova/AppProvaApplication.java
@@ -0,0 +1,13 @@
+package br.com.insper.appprova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppProvaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/insper/appprova/AppProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insper.appprova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
